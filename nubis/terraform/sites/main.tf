b6cf7455c613eac4c3a9cd77e90702caee862ec5
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-03-31 12:03:13 -0400

    tf++

--- nubis/terraform/sites/main.tf
+++ nubis/terraform/sites/main.tf
@@ -1,3 +1,28 @@
+# Discover Consul settings
+module "consul" {
+  source       = "github.com/nubisproject/nubis-terraform//consul?ref=v1.4.0"
+  region       = "${var.region}"
+  environment  = "${var.environment}"
+  account      = "${var.account}"
+  service_name = "${var.service_name}"
+}
+
+# Configure our Consul provider, module can't do it for us
+provider "consul" {
+  address    = "${module.consul.address}"
+  scheme     = "${module.consul.scheme}"
+  datacenter = "${module.consul.datacenter}"
+}
+
+# Publish our outputs into Consul for our application to consume
+resource "consul_keys" "config" {
+  key {
+    path   = "${module.consul.config_prefix}/sites/${var.site_name}/bucket"
+    value  = "http://${module.bucket.website_endpoint}/"
+    delete = true
+  }
+}
+
 module "bucket" {
   source       = "github.com/nubisproject/nubis-terraform//bucket?ref=develop"
   region       = "${var.region}"
