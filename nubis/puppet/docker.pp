62c1464da26a19bb294806b07b23e449e019e6a4
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-14 12:25:09 -0400

    try and order ci => docker

--- nubis/puppet/docker.pp
+++ nubis/puppet/docker.pp
@@ -1,7 +1,10 @@
-#class { 'docker':
-#  proxy => "http://proxy.service.consul:3128/",
-#  bip   => "172.17.42.1/16",
-#}
+class { 'docker':
+  proxy => "http://proxy.service.consul:3128/",
+  bip   => "172.17.42.1/16",
+  requires => [
+    Class['jenkins'],
+  ]
+}
 
 file { '/etc/dnsmasq.d/docker.conf':
   ensure  => file,
