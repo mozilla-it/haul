14aa94d55f98714220d0eebfcdddcbd45f0b752b
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-14 12:14:12 -0400

    try and ensure we have an uptodate apt-update

--- nubis/puppet/ci.pp
+++ nubis/puppet/ci.pp
@@ -2,10 +2,13 @@ package { 'daemon':
   ensure => 'present'
 }->
 class { 'jenkins':
-#  version            => '2.46.1',
+  version            => '2.46.1',
   configure_firewall => false,
   service_enable     => false,
   service_ensure     => 'stopped',
+  require => [
+    Class['Apt::Update'],
+  ],
   config_hash        => {
     'JAVA_ARGS' => {
       'value' => '-Djava.awt.headless=true -Dhudson.diyChunking=false -Dhttp.proxyHost=proxy.service.consul -Dhttp.proxyPort=3128 -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128'
