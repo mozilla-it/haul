3d2524053d50881a291fc615fec54045fd2d707b
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-14 12:18:29 -0400

    unpin

--- nubis/puppet/ci.pp
+++ nubis/puppet/ci.pp
@@ -2,13 +2,10 @@ package { 'daemon':
   ensure => 'present'
 }->
 class { 'jenkins':
-  version            => '2.46.1',
+#  version            => '2.46.1',
   configure_firewall => false,
   service_enable     => false,
   service_ensure     => 'stopped',
-  require => [
-    Class['Apt::Update'],
-  ],
   config_hash        => {
     'JAVA_ARGS' => {
       'value' => '-Djava.awt.headless=true -Dhudson.diyChunking=false -Dhttp.proxyHost=proxy.service.consul -Dhttp.proxyPort=3128 -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128'
