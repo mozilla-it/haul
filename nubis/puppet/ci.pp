ecdc3a681bc625a0e2955b394dc2a59509b9e675
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-18 10:30:09 -0400

    workaround jenkisn and apt bug

--- nubis/puppet/ci.pp
+++ nubis/puppet/ci.pp
@@ -1,18 +1,20 @@
 package { 'daemon':
   ensure => 'present'
+}->
+class { 'jenkins':
+  # Direct download because something in apt-land is borked with varnish ;-(
+  direct_download    => 'https://pkg.jenkins.io/debian-stable/binary/jenkins_2.46.1_all.deb',
+  repo               => false,
+  version            => 'latest',
+  configure_firewall => false,
+  service_enable     => false,
+  service_ensure     => 'stopped',
+  config_hash        => {
+    'JAVA_ARGS' => {
+      'value' => '-Djava.awt.headless=true -Dhudson.diyChunking=false -Dhttp.proxyHost=proxy.service.consul -Dhttp.proxyPort=3128 -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128'
+    },
+  },
 }
-
-#class { 'jenkins':
-#  version            => '2.46.1',
-#  configure_firewall => false,
-#  service_enable     => false,
-#  service_ensure     => 'stopped',
-#  config_hash        => {
-#    'JAVA_ARGS' => {
-#      'value' => '-Djava.awt.headless=true -Dhudson.diyChunking=false -Dhttp.proxyHost=proxy.service.consul -Dhttp.proxyPort=3128 -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128'
-#    },
-#  },
-#}
 
 package { 'awscli':
   ensure => latest,
