904bd1831c90038787ce955d35eb4bd2a9e1584d
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-18 13:00:51 -0400

    fix networking some

--- nubis/puppet/docker.pp
+++ nubis/puppet/docker.pp
@@ -10,3 +10,11 @@ file { '/etc/dnsmasq.d/docker.conf':
   mode    => '0644',
   content => 'interface=docker0',
 }
+
+file { '/etc/network/interfaces.d/dns.cfg':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => 'append domain-name-servers 172.17.42.1',
+}
