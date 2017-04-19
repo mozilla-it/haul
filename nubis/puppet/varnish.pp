82342f2dd223c7fa57462e2c4fdb23a0e2d716bb
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-19 15:20:23 -0400

    Varnish tweaks

--- nubis/puppet/varnish.pp
+++ nubis/puppet/varnish.pp
@@ -2,6 +2,14 @@ class {'varnish':
   varnish_listen_port => 82,
   storage_type          => 'file',
   varnish_storage_size  => '80%',
+  cookiekeeps => [
+    'JSESSIONID[^=]*',
+    'jenkins[^=]*',
+  ],
+  logrealip => true,
+  honor_backend_ttl => true,
+  x_forwarded_proto => true,
+  cond_requests => true,
 }
 
 class { 'varnish::vcl':
