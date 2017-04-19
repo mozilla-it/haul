9a1f047946a5c949e219f6c485ef0a19e48eb025
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-19 14:42:39 -0400

    add missing sites

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -1,11 +1,11 @@
 $docroot = '/var/www/html'
 $access_log_format = '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"'
 
-apache::vhost { 'tlscanary':
+apache::vhost { 'archive':
   port               => $port,
-  servername         => 'tlscanary.mozilla.org',
+  servername         => 'website-archive.mozilla.org',
   serveraliases      => [
-    'tlscanary.allizom.org',
+    'website-archive.allizom.org',
   ],
 
   docroot            => $docroot,
@@ -14,7 +14,6 @@ apache::vhost { 'tlscanary':
     'Remote_Addr 127\.0\.0\.1 internal',
     'Remote_Addr ^10\. internal',
   ],
-
   access_log_env_var => '!internal',
   access_log_format  => $access_log_format,
 
@@ -23,13 +22,53 @@ apache::vhost { 'tlscanary':
     "set X-Nubis-Project ${project_name}",
     "set X-Nubis-Build   ${packer_build_name}",
   ],
-
   rewrites           => [
-    {
-      comment      => 'Proxy to our bucket',
-      rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-      rewrite_rule => ['/(.*) ${sitemap:tlscanary}$1 [P,L]'],
-    }
+      {
+         comment      => 'Proxy to our bucket',
+         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+         rewrite_rule => ['/(.*) ${sitemap:archive}$1 [P,L]'],
+      }
+  ]
+}
+
+apache::vhost { 'bugzilla':
+  port               => $port,
+  servername         => 'www.bugzilla.org',
+  serveraliases      => [
+    'bugzilla.org',
+    'bugzilla.org.uk',
+    'www.bugzilla.org.uk',
+    'virtual-bzorg.mozilla.org',
+    'website-beta.bugzilla.org',
+    'bugzilla.mozilla.osuosl.org',
+  ],
+
+  docroot            => $docroot,
+
+  setenvif           => [
+    'Remote_Addr 127\.0\.0\.1 internal',
+    'Remote_Addr ^10\. internal',
+  ],
+  access_log_env_var => '!internal',
+  access_log_format  => $access_log_format,
+
+  headers            => [
+    "set X-Nubis-Version ${project_version}",
+    "set X-Nubis-Project ${project_name}",
+    "set X-Nubis-Build   ${packer_build_name}",
+
+    #Bug 1317833 Observatory
+    'set X-Content-Type-Options "nosniff"',
+    'set X-XSS-Protection "1; mode=block"',
+    'set X-Frame-Options "DENY"',
+    'set Strict-Transport-Security "max-age=31536000"',
+  ],
+  rewrites           => [
+      {
+         comment      => 'Proxy to our bucket',
+         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+         rewrite_rule => ['/(.*) ${sitemap:bugzilla}$1 [P,L]'],
+      }
   ]
 }
 
@@ -60,9 +99,12 @@ apache::vhost { 'nightly':
   ]
 }
 
-apache::vhost { 'archive':
+apache::vhost { 'publicsuffix':
   port               => $port,
-  servername         => 'website-archive.mozilla.org',
+  servername         => 'www.publicsuffix.org',
+  serveraliases      => [
+    'publicsuffix.org',
+  ],
 
   docroot            => $docroot,
 
@@ -82,8 +124,97 @@ apache::vhost { 'archive':
       {
          comment      => 'Proxy to our bucket',
          rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:archive}$1 [P,L]'],
+         rewrite_rule => ['/(.*) ${sitemap:publicsuffix}$1 [P,L]'],
       }
+  ]
+}
+
+apache::vhost { 'services':
+  port               => $port,
+  servername         => 'www.publicsuffix.org',
+  serveraliases      => [
+    'publicsuffix.org',
+  ],
+
+  docroot            => $docroot,
+
+  setenvif           => [
+    'Remote_Addr 127\.0\.0\.1 internal',
+    'Remote_Addr ^10\. internal',
+  ],
+  access_log_env_var => '!internal',
+  access_log_format  => $access_log_format,
+
+  headers            => [
+    "set X-Nubis-Version ${project_version}",
+    "set X-Nubis-Project ${project_name}",
+    "set X-Nubis-Build   ${packer_build_name}",
+  ],
+  rewrites           => [
+      {
+         comment      => 'Proxy to our bucket',
+         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+         rewrite_rule => ['/(.*) ${sitemap:services}$1 [P,L]'],
+      }
+  ]
+}
+
+apache::vhost { 'sso':
+  port               => $port,
+  servername         => 'sso.mozilla.com',
+
+  docroot            => $docroot,
+
+  setenvif           => [
+    'Remote_Addr 127\.0\.0\.1 internal',
+    'Remote_Addr ^10\. internal',
+  ],
+  access_log_env_var => '!internal',
+  access_log_format  => $access_log_format,
+
+  headers            => [
+    "set X-Nubis-Version ${project_version}",
+    "set X-Nubis-Project ${project_name}",
+    "set X-Nubis-Build   ${packer_build_name}",
+  ],
+  rewrites           => [
+      {
+         comment      => 'Proxy to our bucket',
+         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+         rewrite_rule => ['/(.*) ${sitemap:sso}$1 [P,L]'],
+      }
+  ]
+}
+
+apache::vhost { 'tlscanary':
+  port               => $port,
+  servername         => 'tlscanary.mozilla.org',
+  serveraliases      => [
+    'tlscanary.allizom.org',
+  ],
+
+  docroot            => $docroot,
+
+  setenvif           => [
+    'Remote_Addr 127\.0\.0\.1 internal',
+    'Remote_Addr ^10\. internal',
+  ],
+
+  access_log_env_var => '!internal',
+  access_log_format  => $access_log_format,
+
+  headers            => [
+    "set X-Nubis-Version ${project_version}",
+    "set X-Nubis-Project ${project_name}",
+    "set X-Nubis-Build   ${packer_build_name}",
+  ],
+
+  rewrites           => [
+    {
+      comment      => 'Proxy to our bucket',
+      rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+      rewrite_rule => ['/(.*) ${sitemap:tlscanary}$1 [P,L]'],
+    }
   ]
 }
 
