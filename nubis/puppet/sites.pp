605c3baa56e33ddb95e9bc9bf0e492c0582176ad
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-20 12:49:26 -0400

    refactor

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -1,34 +1,42 @@
 $docroot = '/var/www/html'
 $access_log_format = '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"'
 
-apache::vhost { 'archive':
-  port               => $port,
-  servername         => 'website-archive.mozilla.org',
+define nubis::static ($servername, $serveraliases=[] ) {
+  apache::vhost { $title:
+    port               => $port,
+    servername         => $servername,
+    serveraliases      => $serveraliases,
+  
+    docroot            => $docroot,
+  
+    setenvif           => [
+      'Remote_Addr 127\.0\.0\.1 internal',
+      'Remote_Addr ^10\. internal',
+    ],
+    access_log_env_var => '!internal',
+    access_log_format  => $access_log_format,
+  
+    headers            => [
+      "set X-Nubis-Version ${project_version}",
+      "set X-Nubis-Project ${project_name}",
+      "set X-Nubis-Build   ${packer_build_name}",
+      "set X-Nubis-Site    $title",
+    ],
+    rewrites           => [
+        {
+           comment      => 'Proxy to our bucket',
+           rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+           rewrite_rule => ["/(.*) \${sitemap:${title}}\$1 [P,L]"],
+        }
+    ]
+  }
+}
+
+nubis::static { 'archive':
+  servername => 'website-archive.mozilla.org',
   serveraliases      => [
     'website-archive.allizom.org',
   ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:archive}$1 [P,L]'],
-      }
-  ]
 }
 
 apache::vhost { 'bugzilla':
