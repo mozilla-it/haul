60ade25388a7348c3073a8a86081474535bf3992
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-20 15:20:36 -0400

    refactor

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -19,13 +19,16 @@ $final_rewrite = [
   }
 ]
 
+$default_rewrites = [
+]
+
 define nubis::static ($servername, $serveraliases=[], $headers=[], $rewrites=[]) {
   apache::vhost { $title:
     port               => $port,
     servername         => $servername,
     serveraliases      => $serveraliases,
   
-    docroot            => $docroot,
+    docroot            => "/data/haul/$title",
   
     setenvif           => [
       'Remote_Addr 127\.0\.0\.1 internal',
@@ -35,7 +38,7 @@ define nubis::static ($servername, $serveraliases=[], $headers=[], $rewrites=[])
     access_log_format  => $access_log_format,
   
     headers            => concat($default_headers, "set X-Nubis-Site $title", $headers),
-    rewrites           => concat($rewrites, $final_rewrite),
+    rewrites           => concat($default_rewrites, $rewrites),
   }
 }
 
@@ -58,245 +61,51 @@ nubis::static { 'bugzilla':
   ],
 }
 
-apache::vhost { 'nightly':
-  port               => $port,
-  servername         => 'nightly.mozilla.org',
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:nightly}$1 [P,L]'],
-      }
-  ]
-}
-
-apache::vhost { 'publicsuffix':
-  port               => $port,
-  servername         => 'www.publicsuffix.org',
-  serveraliases      => [
-    'publicsuffix.org',
-  ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:publicsuffix}$1 [P,L]'],
-      }
-  ]
-}
-
-apache::vhost { 'services':
-  port               => $port,
-  servername         => 'www.publicsuffix.org',
-  serveraliases      => [
-    'publicsuffix.org',
-  ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:services}$1 [P,L]'],
-      }
-  ]
-}
-
-apache::vhost { 'sso':
-  port               => $port,
-  servername         => 'sso.mozilla.com',
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:sso}$1 [P,L]'],
-      }
-  ]
-}
-
-apache::vhost { 'tlscanary':
-  port               => $port,
-  servername         => 'tlscanary.mozilla.org',
+nubis::static { 'tlscanary':
+  servername => 'tlscanary.mozilla.org',
   serveraliases      => [
     'tlscanary.allizom.org',
   ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-
-    #Bug 1317833 Observatory
-    'set X-Content-Type-Options "nosniff"',
-    'set X-XSS-Protection "1; mode=block"',
-    'set X-Frame-Options "DENY"',
-    'set Strict-Transport-Security "max-age=31536000"',
-  ],
-
-  rewrites           => [
-    {
-      comment      => 'Proxy to our bucket',
-      rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-      rewrite_rule => ['/(.*) ${sitemap:tlscanary}$1 [P,L]'],
-    }
-  ]
 }
 
-apache::vhost { 'trackertest':
-  port               => $port,
-  servername         => 'trackertest.org',
+nubis::static { 'nightly':
+  servername => 'nightly.mozilla.org',
+  serveraliases      => [
+    'nightly.allizom.org',
+  ],
+}
+
+nubis::static { 'publicsuffix':
+  servername => 'www.publicsuffix.org',
+  serveraliases      => [
+    'publicsuffix.org',
+  ],
+}
+
+nubis::static { 'services':
+  servername => 'docs.services.mozilla.com',
+}
+
+nubis::static { 'sso':
+  servername => 'sso.mozilla.com',
+}
+
+nubis::static { 'trackertest':
+  servername => 'trackertest.org',
   serveraliases      => [
     'itsatracker.org',
     'itsatracker.com',
   ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:trackertest}$1 [P,L]'],
-      }
-  ]
 }
 
-apache::vhost { 'seamonkey':
-  port               => $port,
-  servername         => 'www.seamonkey-project.org',
+nubis::static { 'seamonkey':
+  servername => 'www.seamonkey-project.org',
   serveraliases      => [
     'www-stage.seamonkey-project.org',
     'seamonkey-project.org',
   ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-    "set X-Nubis-Site    seamonkey",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:seamonkey}$1 [P,L]'],
-      }
-  ]
 }
 
-apache::vhost { 'krakenbenchmark':
-  port               => $port,
-  servername         => 'krakenbenchmark.mozilla.org',
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-    "set X-Nubis-Site    krakenbenchmark",
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:krakenbenchmark}$1 [P,L]'],
-      }
-  ]
+nubis::static { 'krakenbenchmark':
+  servername => 'krakenbenchmark.mozilla.org',
 }
