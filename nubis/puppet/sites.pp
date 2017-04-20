ea57c039a64e40a455a7d981913adafef576efb5
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-20 13:17:23 -0400

    more refactor

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -1,7 +1,26 @@
 $docroot = '/var/www/html'
 $access_log_format = '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"'
 
-define nubis::static ($servername, $serveraliases=[] ) {
+$default_headers = [
+  "set X-Nubis-Version ${project_version}",
+  "set X-Nubis-Project ${project_name}",
+  "set X-Nubis-Build   ${packer_build_name}",
+  "set X-Nubis-Site    $title",
+  'set X-Content-Type-Options "nosniff"',
+  'set X-XSS-Protection "1; mode=block"',
+  'set X-Frame-Options "DENY"',
+  'set Strict-Transport-Security "max-age=31536000"',
+]
+
+$final_rewrite = [
+  {
+    comment      => 'Proxy to our bucket',
+    rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
+    rewrite_rule => ["/(.*) \${sitemap:${title}}\$1 [P,L]"],
+  }
+]
+
+define nubis::static ($servername, $serveraliases=[], $headers=[], $rewrites=[]) {
   apache::vhost { $title:
     port               => $port,
     servername         => $servername,
@@ -16,19 +35,8 @@ define nubis::static ($servername, $serveraliases=[] ) {
     access_log_env_var => '!internal',
     access_log_format  => $access_log_format,
   
-    headers            => [
-      "set X-Nubis-Version ${project_version}",
-      "set X-Nubis-Project ${project_name}",
-      "set X-Nubis-Build   ${packer_build_name}",
-      "set X-Nubis-Site    $title",
-    ],
-    rewrites           => [
-        {
-           comment      => 'Proxy to our bucket',
-           rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-           rewrite_rule => ["/(.*) \${sitemap:${title}}\$1 [P,L]"],
-        }
-    ]
+    headers            => concat($default_headers, $headers),
+    rewrites           => concat($rewrites, $final_rewrite),
   }
 }
 
@@ -39,9 +47,8 @@ nubis::static { 'archive':
   ],
 }
 
-apache::vhost { 'bugzilla':
-  port               => $port,
-  servername         => 'www.bugzilla.org',
+nubis::static { 'bugzilla':
+  servername => 'www.bugzilla.org',
   serveraliases      => [
     'bugzilla.org',
     'bugzilla.org.uk',
@@ -50,34 +57,6 @@ apache::vhost { 'bugzilla':
     'website-beta.bugzilla.org',
     'bugzilla.mozilla.osuosl.org',
   ],
-
-  docroot            => $docroot,
-
-  setenvif           => [
-    'Remote_Addr 127\.0\.0\.1 internal',
-    'Remote_Addr ^10\. internal',
-  ],
-  access_log_env_var => '!internal',
-  access_log_format  => $access_log_format,
-
-  headers            => [
-    "set X-Nubis-Version ${project_version}",
-    "set X-Nubis-Project ${project_name}",
-    "set X-Nubis-Build   ${packer_build_name}",
-
-    #Bug 1317833 Observatory
-    'set X-Content-Type-Options "nosniff"',
-    'set X-XSS-Protection "1; mode=block"',
-    'set X-Frame-Options "DENY"',
-    'set Strict-Transport-Security "max-age=31536000"',
-  ],
-  rewrites           => [
-      {
-         comment      => 'Proxy to our bucket',
-         rewrite_map  => [ 'sitemap txt:/etc/haul/sitemap.txt' ],
-         rewrite_rule => ['/(.*) ${sitemap:bugzilla}$1 [P,L]'],
-      }
-  ]
 }
 
 apache::vhost { 'nightly':
