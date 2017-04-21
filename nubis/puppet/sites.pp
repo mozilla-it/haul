e8a6efc7d8a304a6cdaf3182366db29a7c2a9eb0
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-21 09:49:06 -0400

    add custom indexes

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -22,21 +22,27 @@ $final_rewrite = [
 $default_rewrites = [
 ]
 
-define nubis::static ($servername, $serveraliases=[], $headers=[], $rewrites=[]) {
+$default_indexes = [
+  'index.html',
+  'index.htm',
+]
+
+define nubis::static ($servername, $serveraliases=[], $indexes=[], $headers=[], $rewrites=[]) {
   apache::vhost { $title:
     port               => $port,
     servername         => $servername,
     serveraliases      => $serveraliases,
-  
+
     docroot            => "/data/haul/$title",
-  
+
+    directoryindex     => join(" ", concat($indexes, $default_indexes)),
     setenvif           => [
       'Remote_Addr 127\.0\.0\.1 internal',
       'Remote_Addr ^10\. internal',
     ],
     access_log_env_var => '!internal',
     access_log_format  => $access_log_format,
-  
+
     headers            => concat($default_headers, "set X-Nubis-Site $title", $headers),
     rewrites           => concat($default_rewrites, $rewrites),
   }
