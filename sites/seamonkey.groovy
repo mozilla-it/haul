ef8e4f90ce6863924ac9863ba85e17ad1144d226
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-21 14:52:11 -0400

    fix proxy env

--- sites/seamonkey.groovy
+++ sites/seamonkey.groovy
@@ -12,7 +12,7 @@ node {
   stage ('Build') {
     // Symlink destination
     sh "ln -sf ../dst src/dest"
-    docker.image('perl:5.22').inside('-u 0:0 -e https_proxy -e HTTPS_PROXY -e http_proxy -e HTTP_PROXY) {
+    docker.image('perl:5.22').inside('-u 0:0 -e https_proxy -e HTTPS_PROXY -e http_proxy -e HTTP_PROXY') {
       sh "apt-get update"
       sh "apt-get -y install rsync xmlto"
       sh "cpanm --notest Template@2.27 Template::Plugin::XML@2.17"
