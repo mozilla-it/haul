a502f91e80012c7829bc7584a020864082b7742e
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-26 15:02:42 -0400

    add env dump

--- sites/krakenbenchmark.groovy
+++ sites/krakenbenchmark.groovy
@@ -4,6 +4,7 @@ def nubisStatic = new org.mozilla.nubis.Static()
 
 node {
    stage('Prep') {
+        sh "env"
         checkout([$class: 'MercurialSCM', clean: true, credentialsId: '', source: 'https://hg.mozilla.org/projects/kraken', subdir: 'src'])
    nubisStatic.prepSite()
    }
