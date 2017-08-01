d036209d19da591bec9f35898696777c16081dc0
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-08-01 12:45:25 -0400

    add admin interface config

--- nubis/puppet/consul.pp
+++ nubis/puppet/consul.pp
@@ -5,3 +5,11 @@ file { '/etc/nubis.d/0-consul-advertise':
   mode   => '0755',
   source => 'puppet:///nubis/files/consul-advertise',
 }
+
+file { '/etc/consul/svc-haul-admin.json':
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0644',
+  source => 'puppet:///nubis/files/svc-haul-admin.json',
+}
