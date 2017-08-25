d6a53126c6bec689f719c9e03c992eed7dc48e94
Daniel Hartnell <danielhartnell@gmail.com>
2017-08-25 09:15:42 -0700

    corrected Planet build rsync path

--- sites/planet.groovy
+++ sites/planet.groovy
@@ -11,19 +11,16 @@ node {
 
   stage ('Build') {
     // Symlink destination
-    sh "ln -sf ../dst src/dest"
     docker.image('dhartnell/mozilla-planet-builder:latest').inside('-u 0:0 -e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY') {
       sh "apt update"
       sh "apt install -y rsync"
       sh "/usr/local/bin/planet-prod.sh"
-      sh "rsync -aq --exclude=.git /data/genericrhel6/src/ dest/"
+      sh "rsync -aq --exclude=.git /data/genericrhel6/src/ dst/"
     }
   }
 
   stage('Sync') {
      nubisStatic.syncSite()
-     // Cleanup symlink
-     sh "rm -f src/dest"
   }
 
 }
