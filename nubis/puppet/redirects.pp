9c8052952cb65a5a2a84936ffb37d6c89c741963
eziegenhorn <eric@ziegenhorns.com>
2017-08-25 11:32:16 -0500

    Add comments to redirects.pp

--- nubis/puppet/redirects.pp
+++ nubis/puppet/redirects.pp
@@ -1,11 +1,12 @@
 # Redirect sites
 
+# Make the Apache config file for redirects
 file { "$::apache::confd_dir/redirects.conf":
     owner => root,
     group => www-data,
     mode => '0750',
     source => "puppet:///nubis/files/redirects.conf",
-} -> apache::custom_config { 'redirects':
+} -> apache::custom_config { 'redirects':   # And include it in the main Apache config
     content       => "Include $::apache::confd_dir/redirects.conf",
     verify_config => false,
 }
