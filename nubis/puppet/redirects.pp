fb3b6e0a7fddfd4821964e77a295794db5ab46ba
eziegenhorn <eric@ziegenhorns.com>
2017-08-25 11:00:24 -0500

    remove ordering that did not work

--- nubis/puppet/redirects.pp
+++ nubis/puppet/redirects.pp
@@ -5,7 +5,6 @@ file { "$::apache::confd_dir/redirects.conf":
     group => www-data,
     mode => '0750',
     source => "puppet:///nubis/files/redirects.conf",
-#    before => Custom_config['redirects'],
 } -> apache::custom_config { 'redirects':
     content       => "Include $::apache::confd_dir/redirects.conf",
     verify_config => false,
