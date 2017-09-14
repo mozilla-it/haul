d2302700d92485d54a7f1091cba0a88cedefd9ac
Daniel Hartnell <danielhartnell@gmail.com>
2017-09-14 08:59:48 -0700

    added Apache configuration for Bugzilla

--- nubis/puppet/static.pp
+++ nubis/puppet/static.pp
@@ -8,35 +8,38 @@ define nubis::static (
   $redirectmatch_regexp=[],
   $redirectmatch_dest=[],
   $rewrites=[],
-  $custom_fragment=undef) {
+  $custom_fragment=undef,
+  $directories=[]) {
 
   apache::vhost { $title:
-    port                    => $port,
-    servername              => $servername,
-    serveraliases           => $serveraliases,
+    port                 => $port,
+    servername           => $servername,
+    serveraliases        => $serveraliases,
 
-    options                 => $options,
+    options              => $options,
 
-    redirectmatch_status    => $redirectmatch_status,
-    redirectmatch_regexp    => $redirectmatch_regexp,
-    redirectmatch_dest      => $redirectmatch_dest,
+    redirectmatch_status => $redirectmatch_status,
+    redirectmatch_regexp => $redirectmatch_regexp,
+    redirectmatch_dest   => $redirectmatch_dest,
 
-    docroot                 => "/data/haul/${title}",
+    docroot              => "/data/haul/${title}",
 
-    directoryindex          => join(concat($indexes, $default_indexes), ' '),
+    directoryindex       => join(concat($indexes, $default_indexes), ' '),
 
-    setenvif                => [
+    directories          => $directories,
+
+    setenvif             => [
       'X_FORWARDED_PROTO https HTTPS=on',
       'Remote_Addr 127\.0\.0\.1 internal',
       'Remote_Addr ^10\. internal',
     ],
-    access_log_env_var      => '!internal',
-    access_log_format       => $access_log_format,
+    access_log_env_var   => '!internal',
+    access_log_format    => $access_log_format,
 
-    headers                 => concat($default_headers, "set X-Nubis-Site ${title}", $headers),
-    rewrites                => concat($default_rewrites, $rewrites),
+    headers              => concat($default_headers, "set X-Nubis-Site ${title}", $headers),
+    rewrites             => concat($default_rewrites, $rewrites),
 
-    custom_fragment         => $custom_fragment,
+    custom_fragment      => $custom_fragment,
   }
 
 }
