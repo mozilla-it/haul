39f9be949e574d853d80f479dd58e2b0b347d66a
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-12-05 09:05:55 -0500

    path fixups

--- sites/ccadb.groovy
+++ sites/ccadb.groovy
@@ -9,7 +9,7 @@ node {
 
   stage ('Build') {
     docker.image('jekyll/builder:3.6.2').inside('-e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY  --volume=$WORKSPACE:/srv/jekyll') {
-      sh "cd /srv/jekyll/src && DEBUG=true /usr/jekyll/bin/jekyll -v"
+      sh "cd /srv/jekyll/src && DEBUG=true su-exec jekyll /usr/jekyll/bin/jekyll -v"
       sh "cd /srv/jekyll/src && DEBUG=true PAGES_REPO_NWO=mozilla/www.ccadb.org /usr/jekyll/bin/jekyll build --verbose  -d ../dst"
     }
   }
