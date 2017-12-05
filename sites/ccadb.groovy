cc17d752201d1440f946f705586159072053bdd1
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-12-05 10:17:58 -0500

    uid fixes

--- sites/ccadb.groovy
+++ sites/ccadb.groovy
@@ -8,7 +8,7 @@ node {
    }
 
   stage ('Build') {
-    docker.image('jekyll/builder:3.6.2').inside('-e CONNECTED=true -e DEBUG=true -e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY  -v $WORKSPACE:/srv/jekyll:rw,z -u 0:0') {
+    docker.image('jekyll/builder:3.6.2').inside('-e CONNECTED=true -e DEBUG=true -e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY -e JEKYLL_UID=$UID -e JEKYLL_GID=${GROUPS[0]} -v $WORKSPACE:/srv/jekyll:rw,z -v $WORKSPACE/vendor/bundle:/usr/local/bundle:rw,z -u 0:0') {
       sh "cd /srv/jekyll/src && /usr/jekyll/bin/entrypoint jekyll -v"
       sh "cd /srv/jekyll/src && PAGES_REPO_NWO=mozilla/www.ccadb.org /usr/jekyll/bin/entrypoint jekyll build --verbose  -d ../dst"
     }
@@ -18,6 +18,3 @@ node {
      nubisStatic.syncSite()
   }
 }
-
-
-/usr/jekyll/bin/entrypoint
