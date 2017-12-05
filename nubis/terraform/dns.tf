10b3370dd3b7b097a16d21573949b8a4ee4e6851
limed <limed@mozilla.com>
2017-12-05 11:31:15 -0800

    output zone_id and nameserver list

--- nubis/terraform/dns.tf
+++ nubis/terraform/dns.tf
@@ -1,5 +1,8 @@
 # Create a delegation set so that we can reuse the same NS for a
 # multiple hosted zones
+#
+# TODO: Fix this once 2.1.0 lands we need to use the delegation set
+#       that we expose in public state
 resource "aws_route53_delegation_set" "haul-delegation" {
   lifecycle {
     create_before_destroy = true
--- nubis/terraform/dns/outputs.tf
+++ nubis/terraform/dns/outputs.tf
@@ -1,3 +1,6 @@
+output "zone_id" {
+  value = "${aws_route53_zone.master_zone.zone_id}"
+}
 
 output "route53_delegation_set" {
   value = "${var.route53_delegation_set}"
