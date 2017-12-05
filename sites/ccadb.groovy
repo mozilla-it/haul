955d96bddd5c139c9c6b60a1ef4625820c25ecae
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-12-05 11:05:55 -0500

    ditch jekyll container in favor of plain ruby

--- sites/ccadb.groovy
+++ sites/ccadb.groovy
@@ -8,9 +8,13 @@ node {
    }
 
   stage ('Build') {
-    docker.image('jekyll/builder:3.6.2').inside('-e CONNECTED=true -e DEBUG=true -e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY -e JEKYLL_UID=114 -e JEKYLL_GID=120 -v $WORKSPACE:/srv/jekyll:rw,z -v $WORKSPACE/vendor/bundle:/usr/local/bundle:rw,z -u 0:0') {
-      sh "cd /srv/jekyll/src && /usr/jekyll/bin/entrypoint jekyll -v"
-      sh "cd /srv/jekyll/src && PAGES_REPO_NWO=mozilla/www.ccadb.org /usr/jekyll/bin/entrypoint jekyll build --verbose  -d ../dst"
+    docker.image('ruby:2.4').inside('-e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY') {
+      sh "set -x"
+      sh "cd src"
+      sh "pwd"
+      sh "bundler install"
+      sh "jekyll --version"
+      sh "jekyll build --verbose  -d ../dst"
     }
   }
 
