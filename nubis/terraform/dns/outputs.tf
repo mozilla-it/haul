ffb5e7bc0e7098c598539390de16baf51bbf6f6b
limed <limed@mozilla.com>
2017-12-05 11:49:53 -0800

    Rename remaining output as well

--- nubis/terraform/dns/outputs.tf
+++ nubis/terraform/dns/outputs.tf
@@ -2,10 +2,10 @@ output "application_zone_id" {
   value = "${aws_route53_zone.master_zone.zone_id}"
 }
 
-output "route53_delegation_set" {
+output "application_route53_delegation_set" {
   value = "${var.route53_delegation_set}"
 }
 
-output "nameservers" {
+output "application_nameservers" {
   value = "${join(",",aws_route53_zone.master_zone.name_servers)}"
 }
