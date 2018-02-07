cecfc26f4247ec6a471fa4c7e32bad6f640303da
Daniel Hartnell <danielhartnell@gmail.com>
2018-02-07 11:50:36 -0800

    adding mx records for ccadb

--- nubis/terraform/dns.tf
+++ nubis/terraform/dns.tf
@@ -21,3 +21,18 @@ module "ccadb_org" {
   # Make sure to construct a unique zone name depending on the environment
   zone_name               = "${var.environment == "prod" ? "ccadb.org" : join(".", list(var.environment, "ccadb.allizom.org"))}"
 }
+
+# Adding MX records for ccadb
+resource "aws_route53_record" "ccadb_mx" {
+  zone_id = "${module.ccadb_org.application_zone_id}"
+  name    = "ccadb.org"
+  type    = "MX"
+
+  records = [
+    "10 mx1.scl3.mozilla.com",
+    "20 mx2.scl3.mozilla.com"
+  ]
+
+  ttl     = "180"
+}
+
