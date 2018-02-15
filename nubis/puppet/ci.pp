97e5be6c9163971fe4c9d8597530498431143201
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-15 09:14:59 -0500

    Upgrade to Jenkins 2.89.4

--- nubis/puppet/ci.pp
+++ nubis/puppet/ci.pp
@@ -1,3 +1,5 @@
+$jenkins_version = '2.89.4'
+
 python::pip { 'awscli':
   ensure  => '1.11.78',
 }
@@ -25,39 +27,17 @@ file { '/etc/nubis.d/99-jenkins':
   source => 'puppet:///nubis/files/jenkins-startup',
 }
 
-$jenkins_version = '2.73.3'
-
-include ::apt
-apt::ppa { 'ppa:openjdk-r/ppa': }
-
-package { 'openjdk':
-  ensure  => latest,
-  name    => 'openjdk-8-jdk',
-  require => [
-    Apt::Ppa['ppa:openjdk-r/ppa'],
-    Class['apt::update'],
-  ],
-}
-  -> exec { 'update-ca-certificates':
-  # Fixed bug in openjdk-8 package not generated trusted CA store for Java ;_(
-  # https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
-  command => '/usr/sbin/update-ca-certificates --verbose --fresh',
-}
-
-# Fix dependency chains for apt update
-Apt::Ppa['ppa:openjdk-r/ppa'] -> Class['apt::update'] -> Package['openjdk']
-
 package { 'daemon':
   ensure => 'present'
-}
-  -> class { 'jenkins':
+}->
+class { 'jenkins':
   # Direct download because something in apt-land is borked with varnish ;-(
   direct_download    => "https://pkg.jenkins.io/debian-stable/binary/jenkins_${jenkins_version}_all.deb",
   repo               => false,
   configure_firewall => false,
   service_enable     => false,
   service_ensure     => 'stopped',
-  install_java       => false,
+  install_java       => true,
   config_hash        => {
     'JENKINS_ARGS' => {
       'value' => '--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT --requestHeaderSize=16384 --prefix=/admin/haul-admin-$(nubis-metadata NUBIS_ENVIRONMENT)'
@@ -66,9 +46,6 @@ package { 'daemon':
       'value' => '-Djenkins.install.runSetupWizard=false -Djava.awt.headless=true -Dhudson.diyChunking=false -Dhttp.proxyHost=proxy.service.consul -Dhttp.proxyPort=3128 -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128'
     },
   },
-  require            => [
-    Package['openjdk'],
-  ],
 }
 
 # Jenkins is already defining the user for this, so cheat
@@ -108,6 +85,18 @@ file { '/var/lib/jenkins/jenkins.security.QueueItemAuthenticatorConfiguration.xm
   group   => 'jenkins',
 
   source  => 'puppet:///nubis/files/jenkins.security.QueueItemAuthenticatorConfiguration.xml',
+
+  require => [
+    Class['jenkins'],
+  ],
+}
+
+file { '/var/lib/jenkins/jenkins.CLI.xml':
+  ensure  => present,
+  owner   => 'jenkins',
+  group   => 'jenkins',
+
+  source  => 'puppet:///nubis/files/jenkins.CLI.xml',
 
   require => [
     Class['jenkins'],
@@ -158,322 +147,4 @@ file { '/var/lib/jenkins/jobs/00-haul/config.xml':
   require => [
     File['/var/lib/jenkins/jobs/00-haul'],
   ],
-}
-
-## Plugins
-
-jenkins::plugin { 'ace-editor':
-  version => '1.1'
-}
-
-jenkins::plugin { 'ant':
-  version => '1.8'
-}
-
-jenkins::plugin { 'antisamy-markup-formatter':
-  version => '1.5'
-}
-
-jenkins::plugin { 'apache-httpcomponents-client-4-api':
-  version => '4.5.3-2.1'
-}
-
-jenkins::plugin { 'authentication-tokens':
-  version => '1.3'
-}
-
-jenkins::plugin { 'authorize-project':
-  version => '1.3.0'
-}
-
-jenkins::plugin { 'bouncycastle-api':
-  version => '2.16.2'
-}
-
-jenkins::plugin { 'branch-api':
-  version => '2.0.18'
-}
-
-jenkins::plugin { 'build-timeout':
-  version => '1.19'
-}
-
-jenkins::plugin { 'cloudbees-folder':
-  version => '6.3'
-}
-
-jenkins::plugin { 'credentials-binding':
-  version => '1.15'
-}
-
-jenkins::plugin { 'display-url-api':
-  version => '2.2.0'
-}
-
-jenkins::plugin { 'docker-commons':
-  version => '1.11'
-}
-
-jenkins::plugin { 'docker-workflow':
-  version => '1.15.1'
-}
-
-jenkins::plugin { 'durable-task':
-  version => '1.17'
-}
-
-jenkins::plugin { 'email-ext':
-  version => '2.61'
-}
-
-jenkins::plugin { 'envinject':
-  version => '2.1.5'
-}
-
-jenkins::plugin { 'envinject-api':
-  version => '1.5'
-}
-
-jenkins::plugin { 'external-monitor-job':
-  version => '1.7'
-}
-
-jenkins::plugin { 'git':
-  version => '3.7.0'
-}
-
-jenkins::plugin { 'git-client':
-  version => '2.7.1'
-}
-
-jenkins::plugin { 'git-server':
-  version => '1.7'
-}
-
-jenkins::plugin { 'github':
-  version => '1.29.0'
-}
-
-jenkins::plugin { 'github-api':
-  version => '1.90'
-}
-
-jenkins::plugin { 'github-branch-source':
-  version => '2.3.2'
-}
-
-jenkins::plugin { 'github-organization-folder':
-  version => '1.6'
-}
-
-jenkins::plugin { 'gradle':
-  version => '1.28'
-}
-
-jenkins::plugin { 'handlebars':
-  version => '1.1.1'
-}
-
-jenkins::plugin { 'icon-shim':
-  version => '2.0.3'
-}
-
-jenkins::plugin { 'jackson2-api':
-  version => '2.8.11.1'
-}
-
-jenkins::plugin { 'job-dsl':
-  version => '1.68'
-}
-
-jenkins::plugin { 'jquery-detached':
-  version => '1.2.1'
-}
-
-jenkins::plugin { 'jsch':
-  version => '0.1.54.1'
-}
-
-jenkins::plugin { 'junit':
-  version => '1.24'
-}
-
-jenkins::plugin { 'ldap':
-  version => '1.19'
-}
-
-jenkins::plugin { 'mailer':
-  version => '1.20'
-}
-
-jenkins::plugin { 'mapdb-api':
-  version => '1.0.9.0'
-}
-
-jenkins::plugin { 'matrix-auth':
-  version => '2.2'
-}
-
-jenkins::plugin { 'matrix-project':
-  version => '1.12'
-}
-
-jenkins::plugin { 'mercurial':
-  version => '2.2'
-}
-
-jenkins::plugin { 'momentjs':
-  version => '1.1.1'
-}
-
-jenkins::plugin { 'pam-auth':
-  version => '1.3'
-}
-
-jenkins::plugin { 'pipeline-build-step':
-  version => '2.7'
-}
-
-jenkins::plugin { 'pipeline-github-lib':
-  version => '1.0'
-}
-
-jenkins::plugin { 'pipeline-graph-analysis':
-  version => '1.6'
-}
-
-jenkins::plugin { 'pipeline-input-step':
-  version => '2.8'
-}
-
-jenkins::plugin { 'pipeline-milestone-step':
-  version => '1.3.1'
-}
-
-jenkins::plugin { 'pipeline-model-api':
-  version => '1.2.7'
-}
-
-jenkins::plugin { 'pipeline-model-declarative-agent':
-  version => '1.1.1'
-}
-
-jenkins::plugin { 'pipeline-model-definition':
-  version => '1.2.7'
-}
-
-jenkins::plugin { 'pipeline-model-extensions':
-  version => '1.2.7'
-}
-
-jenkins::plugin { 'pipeline-rest-api':
-  version => '2.9'
-}
-
-jenkins::plugin { 'pipeline-stage-step':
-  version => '2.3'
-}
-
-jenkins::plugin { 'pipeline-stage-tags-metadata':
-  version => '1.2.7'
-}
-
-jenkins::plugin { 'pipeline-stage-view':
-  version => '2.9'
-}
-
-jenkins::plugin { 'plain-credentials':
-  version => '1.4'
-}
-
-jenkins::plugin { 'resource-disposer':
-  version => '0.8'
-}
-
-jenkins::plugin { 'reverse-proxy-auth-plugin':
-  version => '1.6.3'
-}
-
-jenkins::plugin { 'role-strategy':
-  version => '2.7.0'
-}
-
-jenkins::plugin { 'scm-api':
-  version => '2.2.6'
-}
-
-jenkins::plugin { 'script-security':
-  version => '1.41'
-}
-
-jenkins::plugin { 'ssh-credentials':
-  version => '1.13'
-}
-
-jenkins::plugin { 'ssh-slaves':
-  version => '1.25.1'
-}
-
-jenkins::plugin { 'subversion':
-  version => '2.10.2'
-}
-
-jenkins::plugin { 'timestamper':
-  version => '1.8.9'
-}
-
-jenkins::plugin { 'token-macro':
-  version => '2.3'
-}
-
-jenkins::plugin { 'windows-slaves':
-  version => '1.3.1'
-}
-
-jenkins::plugin { 'workflow-aggregator':
-  version => '2.5'
-}
-
-jenkins::plugin { 'workflow-api':
-  version => '2.25'
-}
-
-jenkins::plugin { 'workflow-basic-steps':
-  version => '2.6'
-}
-
-jenkins::plugin { 'workflow-cps':
-  version => '2.44'
-}
-
-jenkins::plugin { 'workflow-cps-global-lib':
-  version => '2.9'
-}
-
-jenkins::plugin { 'workflow-durable-task-step':
-  version => '2.18'
-}
-
-jenkins::plugin { 'workflow-job':
-  version => '2.17'
-}
-
-jenkins::plugin { 'workflow-multibranch':
-  version => '2.17'
-}
-
-jenkins::plugin { 'workflow-scm-step':
-  version => '2.6'
-}
-
-jenkins::plugin { 'workflow-step-api':
-  version => '2.14'
-}
-
-jenkins::plugin { 'workflow-support':
-  version => '2.18'
-}
-
-jenkins::plugin { 'ws-cleanup':
-  version => '0.34'
 }
--- nubis/puppet/files/security.groovy
+++ nubis/puppet/files/security.groovy
@@ -6,23 +6,11 @@ import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 
-println "--> set authorization strategy"
-def strategy = new GlobalMatrixAuthorizationStrategy()
-
-strategy.add(Jenkins.ADMINISTER, "admin")
-strategy.add(Jenkins.ADMINISTER, "authenticated")
-strategy.add(Jenkins.READ, "anonymous")
-
-instance.setAuthorizationStrategy(strategy)
-
 println "--> creating proxy configuration"
 def proxy = new hudson.ProxyConfiguration('proxy.service.consul', 3128, '', '', '127.0.0.1,169.254.169.254')
 instance.proxy = proxy
 
 println "--> Enable Slave to Master Access Control" 
 instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
-
-println "--> Disable CLI remoting"
-instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
 
 instance.save()
