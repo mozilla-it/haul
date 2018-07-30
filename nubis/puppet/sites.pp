88aa08d615a5d9809f873bf7d341b3aa8a244b54
joeyk <jkrejci@mozilla.com>
2018-07-30 11:44:51 -0700

    dynamic net to org change

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -341,7 +341,7 @@ nubis::static { 'mozqa':
 }
 
 nubis::static { 'dynamicua':
-  servername    => 'dynamicua-origin.cdn.mozilla.net',
+  servername    => 'dynamicua-origin.cdn.mozilla.org',
   serveraliases => [
     'dynamicua.allizom.org',
   ],
