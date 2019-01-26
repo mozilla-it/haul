c13b0f5e8080bd6a05836d1aa629e246a376921d
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-01-26 16:18:00 -0500

    Upgrade to Traefik 1.7.7

--- nubis/puppet/traefik.pp
+++ nubis/puppet/traefik.pp
@@ -1,4 +1,4 @@
-$traefik_version = '1.5.4'
+$traefik_version = '1.7.7'
 $traefik_url = "https://github.com/containous/traefik/releases/download/v{${traefik_version}}/traefik_linux-amd64"
 
 notice ("Grabbing traefik ${traefik_version}")
