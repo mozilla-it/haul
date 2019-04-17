fe859165d2b78e666878c5b79d4214e98678f838
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-04-17 15:38:21 -0400

    tyop

--- sites/iot.groovy
+++ sites/iot.groovy
@@ -3,10 +3,6 @@
 def nubisStatic = new org.mozilla.nubis.Static()
 
 node {
-  environment {
-      JEKYLL_UID = "${env.UID}"
-      JEKYLL_GID = "${env.GID}"
-   } 
    stage('Prep') {
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'src/'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: true], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mozilla-iot/mozilla-iot.github.io.git']]])
     checkout([$class: 'GitSCM', branches: [[name: '*/gh-pages']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'src/wot/'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: true], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mozilla-iot/wot.git']]])
@@ -18,7 +14,7 @@ checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleC
   stage ('Build') {
     
     dir("docs") {
-      docker.image('jekyll/jekyll:3.7').inside('-u 0:0 -e JEKYLL_UID=' + env.UID + ' -e JEKYLL_GID=' + env.GID +' --volume="${PWD}:/srv/jekyll" -e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY') {
+      docker.image('jekyll/jekyll:3.7').inside('-u 0:0 -e JEKYLL_UID=' + "${env.UID}" + ' -e JEKYLL_GID=' + "${env.GID}" +' --volume="${PWD}:/srv/jekyll" -e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY') {
         sh "env"
         sh "bundler install"
         sh "/usr/jekyll/bin/entrypoint jekyll build"
